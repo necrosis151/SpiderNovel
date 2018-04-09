251a9206f3d935ee116ddafa46a3942abee1d2a1
necrosis151 <36763986+necrosis151@users.noreply.github.com>
2018-04-09 22:08:03 +0800

    test

--- spider/src/main/java/com/novel/serverimpl/AbstractSpider.java
+++ spider/src/main/java/com/novel/serverimpl/AbstractSpider.java
@@ -1,9 +1,12 @@
 package com.novel.serverimpl;
 
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
@@ -12,12 +15,17 @@ public class AbstractSpider {
     /*
      * 从URL中获取html文本
      * */
-    public String getHtml(String url,String charset) {
+    public String getHtml(String url, String charset) {
         String result = "";
-        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        CloseableHttpResponse httpResponse = null;
+//        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+//        CloseableHttpResponse httpResponse = null;
+        RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.IGNORE_COOKIES).build();
+        CloseableHttpClient client = HttpClients.custom().setDefaultRequestConfig(globalConfig).build();
+        HttpGet request = new HttpGet(url);
+
         try {
-            httpResponse = httpClient.execute(new HttpGet(url));
+            CloseableHttpResponse httpResponse = client.execute(request);
+//            httpResponse = httpClient.execute(new HttpGet(url));
             result = EntityUtils.toString(httpResponse.getEntity(), charset);
         } catch (IOException e) {
             e.printStackTrace();
